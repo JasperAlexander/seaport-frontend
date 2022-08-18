2989d23be642c52e17a3b99e93b4bda07f98be53
Jasper Alexander <84851956+JasperAlexander@users.noreply.github.com>
2022-08-18 22:35:39 +0200

    Updated 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,42 @@
-
+import { useRouter } from 'next/router'
+import { Box } from '../components/Box/Box'
+import { MainButton } from '../components/Buttons/MainButton'
 
 export default function Custom404() {
+    const router = useRouter()
+
     return (
-        <p>Something went wrong</p>
+        <Box
+            display='flex'
+            flexDirection='column'
+            justifyContent='center'
+            alignItems='center'
+            style={{width: '80%'}}
+            marginY='30'
+            marginX='auto'
+        >
+            <Box
+                as='h1'
+                fontWeight='600'
+                fontSize='40'
+            >
+                Oops, something went wrong
+            </Box>
+            <Box
+                fontWeight='400'
+                fontSize='24'
+                color='boxText'
+                textAlign='center'
+                marginY='24'
+            >
+                Yikes, looks like something went wrong on our end. If the issue persists, please shoot us a note so we can help out.
+            </Box>
+            <MainButton
+                href='/'
+            >
+                Try going back to homepage
+            </MainButton>
+        </Box>
+
     )
 }
